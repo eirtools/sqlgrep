ac5656261d4fa8843b9891aa08c63de82434ecd4
Eir Nym <485399+eirnym@users.noreply.github.com>
2025-04-15 18:52:49 +0200

    Select from specific tables

--- src/args.rs
+++ src/args.rs
@@ -1,17 +1,27 @@
+use clap::{ArgAction, Parser};
 use indoc::indoc;
 
-#[derive(clap::Parser, Debug)]
+pub(crate) fn parse_args() -> Args {
+    Args::parse()
+}
+
+#[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 pub(crate) struct Args {
-    #[arg(help="Increase verbosity")]
-    #[arg(short='q', long="quiet")]
-    #[arg(action=clap::ArgAction::Count)]
+    #[arg(help = "Decrease verbosity")]
+    #[arg(short = 'q', long = "quiet")]
+    #[arg(action=ArgAction::Count)]
     pub(crate) quiet: u8,
 
-    #[arg(help="Increase verbosity")]
-    #[arg(short='v', long="verbose")]
-    #[arg(action=clap::ArgAction::Count)]
+    #[arg(help = "Increase verbosity")]
+    #[arg(short = 'v', long = "verbose")]
+    #[arg(action=ArgAction::Count)]
     pub(crate) verbose: u8,
+
+    #[arg(short = 't', long = "table")]
+    #[arg(help = "Table or views to query. Can be used multiple times.")]
+    #[arg(action=ArgAction::Set)]
+    pub(crate) table: Option<Vec<String>>,
 
     #[arg(help = "Pattern to match every cell with")]
     pub(crate) pattern: String,
